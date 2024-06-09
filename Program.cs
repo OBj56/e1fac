cb3e75e53dcc32de8e8e2eac4b813b46cab723e4
musa islam <hamerofth@gmail.com>
2024-06-09 11:53:30 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using static System.Console;
+using static System.Math;
+
+class Hurricane
+{
+    static void Main()
+    {
+        Write("Enter wind speed in Miles Per Hour: ");
+        string input = ReadLine();
+
+        string winds = Regex.Match(input, @"\d+").Value;
+
+        if (int.TryParse(winds, out int windSpeed))
+        {
+
+
+            if (windSpeed >= 157)
+            {
+                Write("Category Five Hurricane");
+            }
+            else if (windSpeed >= 130)
+            {
+                Write("Category Four Hurricane");
+            }
+            else if (windSpeed >= 111)
+            {
+                Write("Category Three Hurricane");
+            }
+            else if (windSpeed >= 96)
+            {
+                Write("Category Two Hurricane");
+            }
+            else if (windSpeed >= 74)
+            {
+                Write("Category One Hurricane");
+            }
+            else
+            {
+                Write("Not a Hurricane");
+            }
+        }
+    }
+}
+    
